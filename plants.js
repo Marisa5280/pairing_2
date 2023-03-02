2c86b9e6b3a770c7445511c018d7ce43e81e81f4
Marisa Wyatt <risawyatt22@gmail.com>
2023-03-01 17:29:24 -0700

    Change the if statement to only require one var

---
+++ plants.js
@@ -0,0 +1,8 @@
+var gets_sunshine = true;
+var ounces_of_water = 6;
+
+if (gets_sunshine === true || ounces_of_water >= 10){
+    console.log ("The plant will LIVE!");
+} else {
+    console.log ("The plant will DIE!");
+}
